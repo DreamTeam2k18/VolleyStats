16b3bef53e59cf653a6c5d54bbca131371f94edc
Emma Thompson <emma.thompson@oit.edu>
2018-02-01 22:11:49 -0800

    Added to Templates
    Added radio buttons for choosing default template type

--- app/src/main/java/com/example/niceg/mysqlproject/TemplatesMenu.java
+++ app/src/main/java/com/example/niceg/mysqlproject/TemplatesMenu.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 public class TemplatesMenu extends AppCompatActivity {
 
@@ -17,4 +19,28 @@ public class TemplatesMenu extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.checkBoxB:
+                if (checked)
+
+                    break;
+            case R.id.checkBoxI:
+                if (checked)
+
+                    // Pirates are the best
+                    break;
+            case R.id.checkBoxC:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
+
+
 }
