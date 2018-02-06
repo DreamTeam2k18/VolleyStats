a14c1ef5b62ba3561eac89f1205d4d8064773c1e
Emma Thompson <emma.thompson@oit.edu>
2018-02-05 20:44:06 -0800

    Added stat taking activity

---
+++ app/src/main/java/com/example/niceg/mysqlproject/TakeStats.java
@@ -0,0 +1,13 @@
+package com.example.niceg.mysqlproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakeStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_stats);
+    }
+}
